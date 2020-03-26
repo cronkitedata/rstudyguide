55c89fa448e0f8a608d59d42c5190b3f6ed1d6c3
Sarah Cohen <sarah.h.cohen@asu.edu>
2020-03-26 18:39:49 -0400

    update plots

--- render_book.R
+++ render_book.R
@@ -4,6 +4,5 @@ render_book("index.Rmd", output_format = "bookdown::gitbook",
             output_dir = "docs",  config_file = "_bookdown.yml")
 
 
-
 # To stage all, go to a terminal window and say git add .
 # Then you can commit. 
